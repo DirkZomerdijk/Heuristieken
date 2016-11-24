b2ccd31d2da07fbe97386da58158170311e70368
lailablomer <laila.blomer@student.uva.nl>
2016-11-24 19:04:38 +0100

    Updated classes, added read-in for netlist1.txt

--- tester.py
+++ tester.py
@@ -1,81 +1,92 @@
 # Creates matrix with plotted net points
 
-GATESFILE = open('gatestest.txt', 'r')
-NETLISTS = open('txtfiles/netlists.txt', 'r')
+GATESFILE = open('gates.txt', 'r')
+NETLISTS = open('txtfiles/netlist1.txt', 'r')
 
-class Grid(object):
-    def __init__(self, width, height, layer):
+class Layer(object):
+    def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.layer = layer
-        self.chip = {}
+        self._grid = {}
         for x in range(0, width):
             for y in range(0, height):
-                self.chip[x, y] = 'free'
+                self._grid[x, y] = 'free'
+    
+    def place(self, item, x, y):
+        self._grid[x, y] = item
 
+    def __str__(self):
+        string = ''
+        for i in range(self.width):
+            for j in range(self.height):
+               string += str(self._grid[i, j]) + ' '
+            string += '\n'
+        return string
 
 # class gate
 class Gate(object):
     def __init__(self, coordinate):
         """
-        grid = grid object, met z = 0
+        layer = layer object, met z = 0
         coordinate = (x,y)
         """
         self.coordinate = coordinate
         self.x = coordinate[0]
         self.y = coordinate[1]
         self.connections = []
-        self.value = 'gate'
+        self.value = '[  ]'
 
     def addConnection(self, connection):
         self.connections.append(connection)
 
-# create board function
-class Board(object):
-    def __init__(self, width, height, GATESFILE):
+    def __str__(self):
+        return self.value
+#
+# class Nets(object):
+#     def __init__(self):
+
+
+
+# create Chip function
+class Chip(object):
+    def __init__(self, width, height, GATESFILE, NETLISTS):
         '''
-        :param grid: Grid object
+        :param layer: Layer object
         :param GATESFILE: file containing all gates
         '''
         self.width = width
         self.height = height
-        self.gates = GATESFILE.readlines()
+        self.gates = [[int(n) for n in line.replace('\n', '').split(',')] for line in GATESFILE.readlines()]
+        self.layers = [Layer(self.width, self.height) for i in range(7)]
+        self.netlist = [[int(n) for n in line.split(',')] for line in NETLISTS.readlines()]
+        self.nets = {}
 
-    def drawGrids(self):
+        self._loadGates()
+
+    def _loadGates(self):
         '''
-
+        read gates from file and make gate objects on layer
         :return:
         '''
-        for layer in range(0,7):
-            Grid(self.width,self.height,layer)
+        for x, y in self.gates:
+            self.layers[0].place(Gate([x, y]), x, y)
 
-    def loadGatesOnGrid(self):
-        '''
-        read gates from file and make gate objects on grid
-        :return:
-        '''
-        for line in self.gates:
-            x, y = line.split(',')
-            Grid(self.width, self.height, 0).chip[x, y[:-1]] = Gate([x, y[:-1]])
-            print Grid(self.width, self.height, 0).chip[x, y[:-1]]
-
-    def printBoard(self):
-        for x in range(0, width):
-            print '\n'
-            for y in range(0, height):
-                print Grid(self.width, self.height, 0).chip[x, y],
+    def printChip(self):
+        for layer in self.layers:
+            print layer
+    #
+    # def placeNet(self):
 
 
 
 
-width = 5
-height = 5
-board = Board(width, height, GATESFILE)
-board.drawGrids()
-board.loadGatesOnGrid()
-board.printBoard()
+width = 13
+height = 18
+Chip = Chip(width, height, GATESFILE, NETLISTS)
+layer = Layer(width,height)
 
-# print board.__dict__
+# PRINT!
+# Chip.printChip()
 
 # def runClasses(width, height, file):
 
